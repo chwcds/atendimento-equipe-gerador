7b90f185fdb0f391cf55b01f7d61362b441f1ba0
chwcds <chwcds@github.com>
2026-08-08 21:12:24 -0300

    Embed static options for technicians in index.html (v22)

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 /**
- * SERVICE WORKER - PWA Offline Cache Manager (v21)
+ * SERVICE WORKER - PWA Offline Cache Manager (v22)
  */
 
-const CACHE_NAME = 'supermarket-checklist-v21';
+const CACHE_NAME = 'supermarket-checklist-v22';
 
 const ASSETS_TO_CACHE = [
   './',
