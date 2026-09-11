f76bfcc30dad850aedad9e56814a4b999dfa7883
chwcds <chwcds@github.com>
2026-09-10 21:20:07 -0300

    Atualiza app para versao 7.3.11 com novo tecnico Pedro Henrique

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 /**
- * SERVICE WORKER - PWA Offline Cache Manager (v7.3.10)
+ * SERVICE WORKER - PWA Offline Cache Manager (v7.3.11)
  */
 
-const CACHE_NAME = 'supermarket-checklist-v7.3.10';
+const CACHE_NAME = 'supermarket-checklist-v7.3.11';
 
 const ASSETS_TO_CACHE = [
   './',
